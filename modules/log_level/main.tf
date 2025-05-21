f1c157d0917f8094b8c7ca3abef16b6c7218fd95
maihg <mai.helene@outlook.com>
2025-05-21 20:17:03 +0200

    Remove unnecessary right parenthesis

--- modules/log_level/main.tf
+++ modules/log_level/main.tf
@@ -26,7 +26,7 @@ resource "datadog_monitor" "too_many_logs_of_log_level" {
   priority = var.priority
 
   query   = "logs(\"${local.log_query}\").index(\"${var.index_to_monitor}\").rollup(\"count\").last(\"${var.period}\") > ${var.alert_threshold}"
-  message = var.workflow_to_attach != null ? var.workflow_to_attach : "@slack-${var.slack_channel_to_notify})"
+  message = var.workflow_to_attach != null ? var.workflow_to_attach : "@slack-${var.slack_channel_to_notify}"
 
   enable_logs_sample       = true
   require_full_window      = false
